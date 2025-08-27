d5f08fc131e4bf35d70c92aa7224e06ecb64c790
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2025-08-27 20:27:25 +0530

    done

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,12 +7,8 @@ router.post("/register", authController.registerUser);
 
 router.post("/login", authController.loginUser);
 
-<<<<<<< HEAD
 router.get("/logout", authUser.isAuthenticated, authController.logoutUserUser);
 
 router.get("/", authUser.isAuthenticated, authController.loggedInUserUser);
-=======
-router.get("/", authUser.isAuthenticated, authController.loggedInUser);
->>>>>>> b9e3f2d4960e0823eec639892d9f8a38ad625201
 
 module.exports = router;
